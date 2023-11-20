0c9348a8e62fbb161b2eb0faa2988b410dd20f61
luigilalo <luigilaloe1@gmail.com>
2023-11-20 17:25:08 +0100

    Some adjustements

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-// chiedo all'utente di inserire una parola
+// PRIMO ESERCIZIO, PAROLA PALINDROMA
+
 /*
+// Chiedo all'utente di inserire una parola
 let usersWord = prompt("Inserisci un parola")
 
-// stamp
- PAROLA PALINDROMA
 console.log(usersWord)
 
 function checkPalindrome (choosenWord) { 
@@ -14,15 +14,16 @@ console.log("La parola è palindroma? " + checkPalindrome(usersWord));
 */
 
 
-// PARI E DISPARI 
 
+
+// SECONDO ESERCIZIO, PARI E DISPARI 
 let oddOrEven = prompt("Scegli se pari o dispari")
 let userNumber =parseInt(prompt("Scegli un numero da 1 a 5"));
 
 console.log(`Hai scelto:${oddOrEven}`);
 console.log(`Hai scelto il numero:${userNumber}`);
 
-/* Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). */
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). //
 function createRandomNumber(min, max){
 
     const newRandomNum = Math.floor(Math.random() * (max - min) + min);
@@ -32,8 +33,7 @@ function createRandomNumber(min, max){
 let randomNumber = createRandomNumber (1, 5);
 console.log(`numero scelto dal computer:${randomNumber}`);
 
-/* Sommiamo stabilendo se la somma dei due numeri è pari o dispari (usando una funzione) */
-
+// Sommiamo stabilendo se la somma dei due numeri è pari o dispari (usando una funzione) //
 function sumElements(element1, element2){
     let sum = element1 + element2;
     if (sum % 2 === 0) {
@@ -44,8 +44,8 @@ function sumElements(element1, element2){
     }
     return sum;
 }
-/* Dichiariamo chi ha vinto. */
 
+// Dichiariamo chi ha vinto. //
 let oddEven = sumElements (userNumber, randomNumber);
 console.log(`risultato somma = ${oddEven}`); 
 
