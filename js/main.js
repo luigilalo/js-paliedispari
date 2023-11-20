f7388e959808b16825fd2231860182589857a781
luigilalo <luigilaloe1@gmail.com>
2023-11-20 16:58:32 +0100

    Started Odd or Even

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // chiedo all'utente di inserire una parola
-
+/*
 let usersWord = prompt("Inserisci un parola")
 
 // stamp
-
+ PAROLA PALINDROMA
 console.log(usersWord)
 
 function checkPalindrome (choosenWord) { 
@@ -11,3 +11,21 @@ function checkPalindrome (choosenWord) {
 }
 
 console.log("La parola è palindroma? " + checkPalindrome(usersWord));
+*/
+
+
+// PARI E DISPARI 
+
+let oddOrEven = prompt("Scegli se pari o dispari")
+let userNumber =parseInt(prompt("Scegli un numero da 1 a 5"));
+
+console.log(`Hai scelto:${oddOrEven}`);
+console.log(`Hai scelto il numero:${userNumber}`);
+
+/* Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). */
+function createRandomNumber(min, max){
+
+    const newRandomNum = Math.floor(Math.random() * (max - min) + min);
+    return newRandomNum
+}
+
